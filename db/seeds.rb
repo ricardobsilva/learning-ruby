4ac807aee9e842a4a0d80a77b7df2476e995681b
Douglas Costa <dodop3000@gmail.com>
2016-01-11 00:12:15 +0000

    Popula o banco com estados e cidades

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Popula o banco com estados e cidades brasileiras
+
+require 'net/http'
+require 'net/https' # for ruby 1.8.7
+require 'json'
+
+# Busca informações de um json com as informações
+def states
+  http = Net::HTTP.new('raw.githubusercontent.com', 443); http.use_ssl = true
+  JSON.parse http.get('/zigotto/br_populate/master/states.json').body
+end
+
+# Confere se a cidade é uma capital
+def capital?(city, state)
+  city == state["capital"]
+end
+
+# Grava as informações no banco
+ActiveRecord::Base.transaction do
+  states.each do |state|
+    state_obj = State.find_or_create_by(acronym: state["acronym"], name: state["name"])
+
+    state["cities"].map { |city| City.find_or_create_by(name: city, state: state_obj, capital: capital?(city, state)) }
+  end
+end
